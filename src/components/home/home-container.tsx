4154505d98c21ac4079f3b200940031374b45565
Yves Wetter <yves@pop-os.localdomain>
2023-02-25 14:49:57 +0100

    created sections

--- src/components/home/home-container.tsx
+++ src/components/home/home-container.tsx
@@ -1,7 +1,15 @@
 import React from "react";
 
 const HomeContainer = () => {
-  return <div>HomeContainer</div>;
+  return (
+    <div>
+      <section className="h-[30vh]">Welcome Section</section>
+      <section className="h-[30vh]">Skills Section</section>
+      <section className="h-[30vh]">Image Section</section>
+      <section className="h-[30vh]">Projects Section</section>
+      <section className="h-[30vh]">Image Section</section>
+    </div>
+  );
 };
 
 export default HomeContainer;
