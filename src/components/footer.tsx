ebd5a9e75e838668c9b46dea7c901eb767fb61c3
Yves Wetter <yves@pop-os.localdomain>
2023-02-25 13:27:44 +0100

    hover icons

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -6,11 +6,12 @@ const Footer = () => {
     <div className="border-t-[1px]">
       <div className="h-32 p-5 container mx-auto">
         <div className="flex justify-center">
-          <AiFillGithub className="h-[30px] w-[30px] mr-12" />
-          <AiFillLinkedin className="h-[30px] w-[30px]" />
+          <AiFillGithub className="transition-all ease-in-out duration-150 h-[30px] w-[30px] mr-20 hover:cursor-pointer hover:scale-125" />
+          <AiFillLinkedin className="transition-all ease-in-out duration-150 h-[30px] w-[30px] hover:cursor-pointer hover:scale-125" />
         </div>
         <div className="flex justify-center pt-4">
-          <p className="r-text-s">{`${new Date().getFullYear()} - Yves Wetter - Impressum`}</p>
+          <span className="r-text-s">{`${new Date().getFullYear()} - Yves Wetter -`}</span>
+          <span className="r-text-s hover:cursor-pointer">{"Impressum"}</span>
         </div>
       </div>
     </div>
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -6,10 +6,14 @@ const Header = () => {
     <div className="shadow-lg">
       <div className="container mx-auto h-[80px] p-5 flex justify-between">
         <div className="flex">
-          <img src={logo_klein} alt="logo" className="w-10 mr-5" />
+          <img
+            src={logo_klein}
+            alt="logo"
+            className="w-10 mr-5 hover:cursor-pointer"
+          />
           <h1 className="r-text-l">YVESWETTER.CH</h1>
         </div>
-        <div className="my-auto">
+        <div className="my-auto hover:cursor-pointer">
           <p className="r-text-m">CONTACT</p>
         </div>
       </div>
