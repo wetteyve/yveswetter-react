184b062ec38dc46c65cf27e9a7c28669abdfbcff
Yves Wetter <yves@pop-os.localdomain>
2023-02-26 15:50:48 +0100

    impressum done

--- src/components/impressum/impressum-container.tsx
+++ src/components/impressum/impressum-container.tsx
@@ -1,5 +1,6 @@
-import React from "react";
 import { Helmet } from "react-helmet";
+import FadeInAnimation from "../fade-in/fadein-animation";
+import data from "./data.json";
 
 const ImpressumContainer = () => {
   const title = "Impressum | Yves Wetter";
@@ -19,7 +20,19 @@ const ImpressumContainer = () => {
         <meta content={description} property="og:description" />
         <meta content="website" property="og:type" />
       </Helmet>
-      ImpressumContainer
+      <FadeInAnimation direction={"up"}>
+        <h1 className="r-text-xl font-semibold pb-6">{data.title}</h1>
+        <div className="grid grid-cols-2">
+          <span>{"Name"}</span>
+          <span>{data.fullName}</span>
+          <span>{"Country"}</span>
+          <span>{data.country}</span>
+          <span>{"Email"}</span>
+          <span>{data.email}</span>
+          <span>{`Copyright © ${new Date().getFullYear()} by Yves Wetter`}</span>
+          <span>{data.disclaimer}</span>
+        </div>
+      </FadeInAnimation>
     </div>
   );
 };
