c43d1dda7c2ae3b608d6b4e842bd1aa766ae389a
Yves Wetter <yves@pop-os.localdomain>
2023-02-28 18:13:49 +0100

    skills animiert

--- src/components/home/skills/skill-item.tsx
+++ src/components/home/skills/skill-item.tsx
@@ -1,19 +1,37 @@
 import './animate-3d.css';
 
+import { useRef } from 'react';
+
+import { roationObserver, useIntersectionObserver } from '../../../utils/intersection-observer';
+
 type SkillItemProps = {
   skillTitle: string;
   skillLogo?: React.ReactNode;
 };
 
 const SkillItem = ({ skillTitle, skillLogo }: SkillItemProps) => {
-  const BLa = skillLogo;
+  const logoRef = useRef<HTMLDivElement>(null);
+  useIntersectionObserver(logoRef, roationObserver);
+
+  const rotate = () => {
+    const element = document.getElementById(skillTitle);
+    if (element?.classList.contains('rotate-logo-forward')) {
+      element?.classList.remove('rotate-logo-forward');
+      element?.classList.add('rotate-logo-backward');
+    } else {
+      element?.classList.remove('rotate-logo-backward');
+      element?.classList.add('rotate-logo-forward');
+    }
+  };
 
   return (
     <div className='flex flex-col items-center mr-10 pb-6'>
       <div>
         <h4 className='pb-2'>{skillTitle}</h4>
       </div>
-      <div className='h-max rotate-vertical'>{skillLogo}</div>
+      <div ref={logoRef} id={skillTitle} className='h-max' onClick={rotate}>
+        {skillLogo}
+      </div>
     </div>
   );
 };
--- src/utils/intersection-observer.tsx
+++ src/utils/intersection-observer.tsx
@@ -1,34 +1,29 @@
-import React, { useEffect } from "react";
+import React, { useEffect } from 'react';
 
 export const fadeInObserver = new IntersectionObserver(
   (entries) => {
     entries
       .filter((e) => e.isIntersecting)
       .forEach((entry, index) => {
-        if (
-          !(entry.target instanceof HTMLElement) &&
-          !(entry.target instanceof SVGElement)
-        )
-          return;
+        if (!(entry.target instanceof HTMLElement) && !(entry.target instanceof SVGElement)) return;
 
-        const origin = entry.target.getAttribute("data-origin") || "left";
-        if (!entry.target.style.animationDelay)
-          entry.target.style.animationDelay = `${800 * index}ms`;
+        const origin = entry.target.getAttribute('data-origin') || 'left';
+        if (!entry.target.style.animationDelay) entry.target.style.animationDelay = `${800 * index}ms`;
         switch (origin) {
-          case "left":
-            entry.target.classList.add("fadeInFromLeft");
+          case 'left':
+            entry.target.classList.add('fadeInFromLeft');
             break;
-          case "right":
-            entry.target.classList.add("fadeInFromRight");
+          case 'right':
+            entry.target.classList.add('fadeInFromRight');
             break;
-          case "top":
-            entry.target.classList.add("fadeInFromTop");
+          case 'top':
+            entry.target.classList.add('fadeInFromTop');
             break;
-          case "bottom":
-            entry.target.classList.add("fadeInFromBottom");
+          case 'bottom':
+            entry.target.classList.add('fadeInFromBottom');
             break;
           default:
-            entry.target.classList.add("fadeInFromLeft");
+            entry.target.classList.add('fadeInFromLeft');
             break;
         }
         fadeInObserver.unobserve(entry.target);
@@ -39,10 +34,22 @@ export const fadeInObserver = new IntersectionObserver(
   }
 );
 
-export const useIntersectionObserver = (
-  elementRef: React.MutableRefObject<Element | undefined | null>,
-  observer: IntersectionObserver
-) => {
+export const roationObserver = new IntersectionObserver(
+  (entries) => {
+    entries
+      .filter((e) => e.isIntersecting)
+      .forEach((entry) => {
+        if (!(entry.target instanceof HTMLElement) && !(entry.target instanceof SVGElement)) return;
+        entry.target.classList.add('rotate-logo-forward');
+        roationObserver.unobserve(entry.target);
+      });
+  },
+  {
+    threshold: 0.3,
+  }
+);
+
+export const useIntersectionObserver = (elementRef: React.MutableRefObject<Element | undefined | null>, observer: IntersectionObserver) => {
   useEffect(() => {
     const currentRef = elementRef.current;
     if (currentRef) observer.observe(currentRef);
