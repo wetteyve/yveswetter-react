cbd9efe9af783d56c5e4ac022fb520c8bc4d8c0c
Yves Wetter <yves@pop-os.localdomain>
2023-03-02 13:30:36 +0100

    contact section fertig

--- src/components/home/contact-section.tsx
+++ src/components/home/contact-section.tsx
@@ -1,9 +1,13 @@
+import Lottie from 'lottie-react';
 import { useRef } from 'react';
+import { useHistory } from 'react-router-dom';
 
+import contactAnimation from '../../assets/contact-lottie.json';
 import { data } from '../../content/home-data';
 import { fadeInObserver, useIntersectionObserver } from '../../utils/intersection-observer';
 
 const ContactSection = () => {
+  const history = useHistory();
   const contactSectionRef = useRef<HTMLDivElement>(null);
   useIntersectionObserver(contactSectionRef, fadeInObserver);
 
@@ -11,6 +15,12 @@ const ContactSection = () => {
     <div ref={contactSectionRef} data-origin='bottom' className='pb-12 opacity-0'>
       <h1 className='r-text-xl font-semibold pb-6'>{data.contactHeading}</h1>
       <p className='r-text-m pb-6'>{data.contactIntroduction}</p>
+      <div
+        onClick={() => history.push('/contact')}
+        className='cursor-pointer transition-all ease-in duration-150 hover:scale-105 max-w-[800px] overflow-hidden mx-auto shadow-2xl rounded-lg'
+      >
+        <Lottie animationData={contactAnimation} loop={true} />
+      </div>
     </div>
   );
 };
