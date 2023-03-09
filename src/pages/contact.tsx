cc8ce873ba9114537d703431359ff556f121feb3
Yves Wetter <yves@pop-os.localdomain>
2023-03-09 08:44:47 +0100

    image test

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -4,7 +4,7 @@ import React, { useEffect, useState } from 'react';
 import ContactForm from '../components/contact/contact-form';
 
 const ContactContainer = () => {
-  const title = 'Contact - Yves Wetter';
+  const title = 'Contact | Yves Wetter';
   const description = 'Get in contact with Yves Wetter.';
   const keywords =
     'yves, wetter, yveswetter, web, fullstack, software, development, engineer, website, webseite, nextjs, react, frontend, backend, devops, portfolio, projects, contact, message, mail, schweiz, switzerland, ch';
